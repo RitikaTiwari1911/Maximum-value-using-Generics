b7ed93ce05424d9bda5f3e05b1695e3142f80fd0
RitikaTiwari1911 <spk2ritika1911@gmail.com>
2021-04-25 00:15:42 +0530

    ADDED: problem statement mentioned

---
+++ src/main/java/com/generics/MaxValueGenerics.java
@@ -0,0 +1,7 @@
+package com.generics;
+
+public class MaxValueGenerics {
+    public static void main(String args[]){
+        System.out.println("Finding the maximum value with Generics !!");
+    }
+}
